4a5ea160ac746686f1aca9f17d54a3027e1cb059
ansh1805sharma-del <ansh1805sharma@gmail.com>
2025-10-08 00:56:15 +0530

    Add files via upload

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,8 +1,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { StreamChat } from "stream-chat";
 import { NextResponse } from 'next/server'; // Import NextResponse for proper API responses
-const api_key = "99ue9bx5k6rp";
-const api_secret = "ycp7sv2dgp7nekpdx2pn63km3kxhfj7bqzmc7f78kc36kf8yxfqq3m9n5uqeudgn";
+
 
 export const dynamic = "force-static";
 export async function GET() {
